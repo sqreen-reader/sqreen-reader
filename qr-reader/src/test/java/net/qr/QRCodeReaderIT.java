36642f34a27987afd1eed7de390f762aa0c1ad9e
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-04 01:00:22 -0400

    Adding IT for when the QR image has more than just QR code

--- qr-reader/src/test/java/net/qr/QRCodeReaderIT.java
+++ qr-reader/src/test/java/net/qr/QRCodeReaderIT.java
@@ -22,4 +22,14 @@ class QRCodeReaderIT {
 
         assertEquals("https://www.geeksforgeeks.org", url);
     }
+
+    @Test
+    @DisplayName("Should read image has more than just QR code")
+    void testReadDirtyRCode() throws IOException {
+        QRCodeReader qrCodeReader = new QRCodeReader(new MultiFormatReader());
+        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/dirty-qr.png"));
+        String url = qrCodeReader.read(image);
+
+        assertEquals("https://www.google.com/", url);
+    }
 }
