4e0cb1a477ec9edeed17840987f3f8cd71d29487
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-04 00:56:59 -0400

    Adding IT test for reading QR codes

---
+++ qr-reader/src/test/java/net/qr/QRCodeReaderIT.java
@@ -0,0 +1,25 @@
+package net.qr;
+
+import com.google.zxing.MultiFormatReader;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class QRCodeReaderIT {
+
+    @Test
+    @DisplayName("Should read QR Code")
+    void testReadCleanQRCode() throws IOException {
+        QRCodeReader qrCodeReader = new QRCodeReader(new MultiFormatReader());
+        BufferedImage image = ImageIO.read(getClass().getResourceAsStream("/test.jpg"));
+        String url = qrCodeReader.read(image);
+
+        assertEquals("https://www.geeksforgeeks.org", url);
+    }
+}
