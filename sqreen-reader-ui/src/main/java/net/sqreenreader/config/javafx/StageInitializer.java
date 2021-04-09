8cd6bb20364640662bce31f1096d96e2f8d733bf
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-08 23:09:04 -0400

    adding icon

--- sqreen-reader-ui/src/main/java/net/sqreenreader/config/javafx/StageInitializer.java
+++ sqreen-reader-ui/src/main/java/net/sqreenreader/config/javafx/StageInitializer.java
@@ -3,6 +3,7 @@ package net.sqreenreader.config.javafx;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -10,6 +11,8 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
+import java.awt.Toolkit;
+import java.awt.Taskbar;
 import java.io.IOException;
 
 @Component
@@ -17,14 +20,16 @@ public class StageInitializer implements ApplicationListener<StageReadyEvent> {
 
     private final ApplicationContext applicationContext;
     private final String title;
+    private final Toolkit toolkit;
 
     private static final int WIDTH = 800;
     private static final int LENGTH = 600;
 
     public StageInitializer(final ApplicationContext applicationContext,
-                            final @Value("${spring.application.name}") String title) {
+                            final @Value("${spring.application.name}") String title, final Toolkit toolkit) {
         this.applicationContext = applicationContext;
         this.title = title;
+        this.toolkit = toolkit;
     }
 
 
@@ -40,9 +45,20 @@ public class StageInitializer implements ApplicationListener<StageReadyEvent> {
             scene.getStylesheets().add(getClass().getResource("/fxml/sqreen-reader.css").toExternalForm());
             stage.setScene(scene);
             stage.setTitle(title);
+            stage.getIcons().add(new Image(getClass().getResourceAsStream("/icon.png")));
+            setTaskbarIcon();
             stage.show();
         } catch (IOException e) {
             throw new ExceptionInInitializerError(e);
+        }
+    }
+
+    private void setTaskbarIcon() {
+        try {
+            Taskbar taskbar = Taskbar.getTaskbar();
+            java.awt.Image image = toolkit.getImage(getClass().getResource("/icon.png"));
+            taskbar.setIconImage(image);
+        } catch (UnsupportedOperationException ignored) {
         }
     }
 }
