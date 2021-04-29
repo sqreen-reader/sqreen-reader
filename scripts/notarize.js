7ddd4324786de3773fc739498dd6362a44d56a93
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-29 09:26:03 -0400

    removing redundant await

--- scripts/notarize.js
+++ scripts/notarize.js
@@ -9,7 +9,7 @@ exports.default = async function notarizing(context) {
 
   const appName = context.packager.appInfo.productFilename;
 
-  return await notarize({
+  return notarize({
     appBundleId: 'net.mporter',
     appPath: `${appOutDir}/${appName}.app`,
     appleId: process.env.APPLEID,
