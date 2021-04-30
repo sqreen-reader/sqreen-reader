7797c271776a9a2fc3282e5e7747f8910e54224d
matthew-js-porter <matthew.js.porter@jpmchase.com>
2021-04-30 01:01:27 -0400

    adding spectron test

--- src/index.js
+++ src/index.js
@@ -30,9 +30,6 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
-  // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
   mainWindow.webContents.on('new-window', (e, url)=>{
     e.preventDefault();
     shell.openExternal(url);
